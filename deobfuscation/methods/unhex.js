d0ee6d9d15bf93e3e8ed1506314419670324eea2
R3tr0 <nikk3695@gmail.com>
2025-07-23 20:16:33 +0300

    Added licensing

--- deobfuscation/methods/unhex.js
+++ deobfuscation/methods/unhex.js
@@ -1,3 +1,13 @@
+/*
+    Copyright (C) 2023 Nikita Retr0-code Korneev
+
+    jsast.js is free software: you can redistribute, modify it
+    under the terms of the MIT License.
+
+    You should have received a copy of MIT License along with jsast.js.
+    If not, see <https://opensource.org/license/MIT.
+*/
+
 import babelTraverse from '@babel/traverse';
 const traverse = babelTraverse.default || babelTraverse;
 
